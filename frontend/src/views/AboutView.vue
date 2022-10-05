deab0707b2e62ec8df50a7ae8df717c7dc91851f
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-10-05 15:40:26 +0900

    Revert "2022-10-05"
    This reverts commit de9ebdb162507fd8d7e16812ff280c9842368cc9.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="Search">
     <Search />
-    <HelloWorld />
   </div>
 </template>
 
 <script>
 import Search from '@/components/SearchBis.vue'
-import Hello from '../components/HelloWorld.vue'
 export default {
   name: 'SearchBis',
   components: {
-    Search,
-    Hello
-}
+    Search
+  }
 }
 
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,17 +2,15 @@
   <div class="home">
     아 헬로우 안쓰네 ㅋㅋ;
     <Login />
-    <Hello />
   </div>
 </template>
 
 <script>
 import Login from '../components/LoginBis.vue'
-import Hello from '../components/HelloWorld.vue'
 export default {
   name: 'HomeBis',
   components: {
-    Login, Hello
+    Login
   }
 }
 </script>
