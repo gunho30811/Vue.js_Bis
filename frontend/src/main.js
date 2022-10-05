9a41b3322f7deafae06279539908fb59909f4896
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-10-05 16:38:52 +0900

    Revert "backup"
    This reverts commit 8301e8c5a0b14171bf8c4c78c01e8f3a1fc14f75.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,7 @@ import router from './router'
 import store from './store'
 // axios 추가
 import axios from 'axios'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
 // axios를 root 전역으로 사용하기 위하여 추가로 component 안에서 axios를 this.$axios로 사용할 수 있습니다.
-const app = createApp(App).use(Quasar, quasarUserOptions)
+const app = createApp(App)
 app.config.globalProperties.$http = axios
 createApp(App).use(store).use(router).mount('#app')
